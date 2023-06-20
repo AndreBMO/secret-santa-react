532e45eb27e62b23962f1ad1cf12716e70285a97
André Oliveira <andreoliveira@MacBook-Pro-de-Andre.local>
2023-06-20 19:57:48 -0300

    Incluindo lógica para clicar no botão e sortear um nome.

--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -1,16 +1,22 @@
-import React from "react";
-import { useNavigate  } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./MainScreen.module.css";
 
 function MainScreen() {
-  const navigate  = useNavigate();
+  const navigate = useNavigate();
+  const [showButton, setShowButton] = useState(true);
+  const [secretSanta, setSecretSanta] = useState(null);
 
   const handleLogoffButton = () => {
     navigate("/");
-  }
+  };
 
   const handleShuffleButtonClick = () => {
-    
+    const participants = ["Person 1", "Person 2", "Person 3", "Person 4"]; // Replace with your participant list
+    const randomIndex = Math.floor(Math.random() * participants.length);
+    const selectedSanta = participants[randomIndex];
+    setSecretSanta(selectedSanta);
+    setShowButton(false);
   };
 
   return (
@@ -19,12 +25,20 @@ function MainScreen() {
         Logoff
       </button>
       <h1 className={styles.title}>Amigo Secreto</h1>
-      <button className={styles.button} onClick={handleShuffleButtonClick}>
-        Clique aqui para sortear seu amigo secreto!
-      </button>
+      {showButton ? (
+        <button
+          className={styles.button}
+          onClick={handleShuffleButtonClick}
+        >
+          Clique aqui para sortear seu amigo secreto!
+        </button>
+      ) : (
+        <p className={styles.message}>
+          Seu Amigo Secreto é: {secretSanta}
+        </p>
+      )}
     </div>
   );
-
 }
 
 export default MainScreen;
